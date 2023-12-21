ca746f05dee71a957f394a66e2ece57a26c70aca
jonathan mac <joshua.mac648@gmail.com>
2023-12-20 19:50:18 -0500

    creating display box

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import InputBox from './components/InputBox/InputBox';
+import Display from './components/Display/Display';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <InputBox/>
+      <Display/>
     </div>
   );
 }
---
+++ to-do/src/components/Display/Display.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Display.css'
+
+function Display() {
+  return (
+    <div className='container'>
+        <div className='display-form'>
+        </div>
+    </div>
+  )
+}
+
+export default Display
